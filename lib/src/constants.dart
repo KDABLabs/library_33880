3022420d118841b11dd156e42a5893bf2b123f15
Filipe Azevedo <filipe.azevedo@kdab.com>
2023-06-20 14:19:53 +0200

    Constants: Add register route

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -1,6 +1,7 @@
 
 class ConstantsRoutes {
   static const String root = '/';
+  static const String register = '/register';
   static const String settings = '/settings';
   static const String books = '/books';
   static const String bookDetails = '/books/book';
