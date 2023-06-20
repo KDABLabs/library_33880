965e2766973d44266e29ef47924beb3750b73134
Filipe Azevedo <filipe.azevedo@kdab.com>
2023-06-20 11:29:01 +0200

    Account: Add color property

--- lib/src/account/account.dart
+++ lib/src/account/account.dart
@@ -1,10 +1,13 @@
 
+import 'dart:ui';
+
 class Account {
-  const Account(this.displayName, this.login, this.password);
+  const Account(this.displayName, this.login, this.password, this.color);
 
   final String displayName;
   final String login;
   final String password;
+  final Color color;
 }
 
 typedef Accounts = List<Account>;
