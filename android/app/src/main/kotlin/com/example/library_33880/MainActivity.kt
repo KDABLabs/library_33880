6f8f043c92875f4878e1d43442f251cc496f1862
Filipe Azevedo <filipe.azevedo@kdab.com>
2023-08-13 20:06:29 +0200

    Add android support
    This require Oracle Java up to 19.0.2.

---
+++ android/app/src/main/kotlin/com/example/library_33880/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.library_33880
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,9 @@ import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
 
 void main() async {
+  // This is needed on Android
+  WidgetsFlutterBinding.ensureInitialized();
+
   // Set up the SettingsController, which will glue user settings to multiple
   // Flutter Widgets.
   final settingsController = SettingsController(SettingsService());
