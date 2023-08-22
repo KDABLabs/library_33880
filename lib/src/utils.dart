f824e245e200b229c8be6a2c33f7bbb7cd18a948
Filipe Azevedo <filipe.azevedo@kdab.com>
2023-08-22 11:57:10 +0200

    Introduce Utils class
    This provide static helpers.

---
+++ lib/src/utils.dart
@@ -0,0 +1,17 @@
+class Utils {
+  static String beautifyTitle(String title) {
+    final int slashIndex = title.lastIndexOf(' / ');
+
+    if (slashIndex != -1) {
+      title = title.replaceRange(slashIndex, slashIndex + 3, '\n');
+    }
+
+    final int dashIndex = title.lastIndexOf(' - ');
+
+    if (dashIndex != -1) {
+      title = title.replaceRange(dashIndex, dashIndex + 3, '\n');
+    }
+
+    return title;
+  }
+}
