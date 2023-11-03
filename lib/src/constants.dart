a9376f8f09a8d688facdc8bf1c521ca0a42d21e9
Filipe Azevedo <filipe.azevedo@kdab.com>
2023-11-03 16:12:31 +0100

    Add new enums

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -1,10 +1,29 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:enum_flag/enum_flag.dart';
 
 class ConstantsRoutes {
-  static const String root = '/';
-  static const String register = '/register';
-  static const String settings = '/settings';
-  static const String reservations = '/reservations';
-  static const String reservationDetails = '/reservations/details';
-  static const String loans = '/loans';
-  static const String loanDetails = '/loans/details';
+  static const String root = '';
+  static const String register = 'register';
+  static const String settings = 'settings';
+  static const String reservations = 'reservations';
+  static const String reservationDetails = 'reservations/details';
+  static const String loans = 'loans';
+  static const String loanDetails = 'loans/details';
+}
+
+enum SearchCriterion with EnumFlag {
+  All, // all
+  Title, // title
+  Author, // author
+  Editor, // editor
+  Subject, // subject
+  Collection, // collection
+  Serie, // serie
+}
+
+enum ResultView {
+  Complete, // complete
+  List, // list
+  Vignette, // vignette
 }
